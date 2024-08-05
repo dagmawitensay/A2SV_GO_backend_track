2e16451a447730f541ca9260e268790692761e02
dagmawitensay <dagitensay@gmail.com>
2024-08-05 22:41:52 +0300

    Add Task 3

---
+++ Task3/library_management/controllers/library_controller.go
@@ -0,0 +1,186 @@
+package controllers
+
+import (
+	"fmt"
+	"library_management/Services"
+	"library_management/models"
+)
+
+type LibraryController struct {
+	Library *services.Library
+}
+
+func NewLibraryController() *LibraryController{
+	return &LibraryController{
+		Library: services.NewLibrary(),
+	}
+}
+
+func (lc *LibraryController) AddBook() {
+	var id int
+	var title string
+
+	for {
+		fmt.Print("Enter Book ID: ")
+		_, err := fmt.Scan(&id)
+		if err != nil {
+			fmt.Println("Invalid Input. Please Enter a valid number")
+			continue
+		}
+		break
+	}
+
+	for {
+		fmt.Print("Enter Book title: ")
+		_, err := fmt.Scan(&title)
+		if err != nil {
+			fmt.Println("Invalid Input. Please Enter a valid title.")
+			continue
+		}
+		break
+	}
+
+	book := models.Book{ID: id, Title: title}
+	lc.Library.AddBook(book)
+	fmt.Println("Book added Successfully!")
+}
+
+func (lc *LibraryController) RemoveBook() {
+	var id int
+
+	for {
+		fmt.Print("Enter Book ID: ")
+		_, err := fmt.Scan(&id)
+		if err != nil {
+			fmt.Println("Invalid Input. Please Enter valid book ID.")
+			continue
+		}
+		break
+	}
+
+	lc.Library.RemoveBook(id)
+	fmt.Println("Book has been removed successfully!")
+}
+
+func (lc *LibraryController) BorrowBook() {
+	var bookID int
+	var memberID int
+
+	for {
+		fmt.Print("Enter member ID: ")
+		_, err := fmt.Scan(&memberID)
+		if err != nil {
+			fmt.Println("Invalid Input. Please enter a valid member id.")
+			continue
+		}
+		break
+	}
+
+	for {
+		fmt.Print("Enter book ID: ")
+		_, err := fmt.Scan(&bookID)
+		if err != nil {
+			fmt.Println("Invalid Input. Please enter a valid book id.")
+			continue
+		}
+		break
+	}
+
+	err := lc.Library.BorrowBook(bookID, memberID)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	} else {
+		fmt.Printf("Book borrowed Successfully!")
+	}
+
+}
+
+func (lc *LibraryController) ReturnBook() {
+	var bookID int
+	var memberID int
+
+	for {
+		fmt.Print("Enter member ID: ")
+		_, err := fmt.Scan(&memberID)
+		if err != nil {
+			fmt.Println("Invalid Input. Please enter a valid member id.")
+			continue
+		}
+		break
+	}
+
+	for {
+		fmt.Print("Enter book ID: ")
+		_, err := fmt.Scan(&bookID)
+		if err != nil {
+			fmt.Println("Invalid Input. Please enter a valid book id.")
+			continue
+		}
+		break
+	}
+
+	err := lc.Library.ReturnBook(bookID, memberID)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	} else {
+		fmt.Printf("Book Returned Successfully!")
+	}
+}
+
+func (lc *LibraryController) ListAvailableBooks() {
+	fmt.Println(lc.Library.ListAvailableBooks())
+}
+
+func (lc *LibraryController) ListBorrowedBooks() {
+	var memberID int
+	for {
+		fmt.Print("Enter member ID: ")
+		_, err := fmt.Scan(&memberID)
+
+		if err != nil {
+			fmt.Println("Invalid Input. Please enter a valid id")
+			continue
+		}
+		break
+	}
+	fmt.Println(lc.Library.ListBorrowedBooks(memberID))
+}
+
+func (lc *LibraryController) ShowMenu() {
+	fmt.Println("Library Management System")
+	fmt.Println("1. Add Book")
+	fmt.Println("2. Remove Book")
+	fmt.Println("3. Borrow Book")
+	fmt.Println("4. Return Book")
+	fmt.Println("5. List Available Books")
+	fmt.Println("6. List Borrowed Books")
+	fmt.Println("7. Add Member")
+	fmt.Println("8. Exit")
+}
+
+func (lc *LibraryController) Run() {
+	for {
+		lc.ShowMenu()
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scan(&choice)
+		switch choice {
+		case 1: 
+			lc.AddBook()
+		case 2:
+			lc.RemoveBook()
+		case 3:
+			lc.BorrowBook()
+		case 4:
+			lc.ReturnBook()
+		case 5:
+			lc.ListAvailableBooks()
+		case 6:
+			lc.ListBorrowedBooks()
+		default:
+			fmt.Println("Invalid choice. Please try again!")
+		}
+	}
+}
---
+++ Task3/library_management/services/library_service.go
@@ -0,0 +1,120 @@
+package services
+
+import (
+	"library_management/models"
+	"errors"
+)
+
+type LibraryManager interface {
+	AddBook(book models.Book)
+	RemoveBook(bookID int)
+	BorrowBook(bookID int, memberID int) error
+	ReturnBook(bookID int, memberID int) error
+	ListAvailableBooks() []models.Book
+	ListBorrowedBooks(memberID int) []models.Book
+}
+
+type Library struct {
+	Books map[int]models.Book
+	Members map[int]models.Member
+}
+
+func NewLibrary() *Library {
+	return &Library{
+		Books : make(map[int]models.Book),
+		Members: make(map[int]models.Member),
+	}
+}
+
+func (l *Library) AddBook(book models.Book) {
+	l.Books[book.ID] = book;
+}
+
+func (l *Library) RemoveBook(bookID int) {
+	delete(l.Books, bookID)
+
+	for memberId, member := range l.Members {
+		for i, borrowedBook := range member.BorrowedBooks {
+			if borrowedBook.ID == bookID {
+				member.BorrowedBooks = append(member.BorrowedBooks[:i], member.BorrowedBooks[i + 1:]...)
+				l.Members[memberId] = member
+				break
+			}
+		}
+	}
+}
+
+func (l *Library) BorrowBook(bookID, memberID int) error {
+	book, ok := l.Books[bookID]
+	if !ok {
+		return errors.New("book not found")
+	}
+
+	member, ok := l.Members[memberID]
+	if !ok {
+		return errors.New("member not found")
+	}
+
+	for _, member := range l.Members {
+		for _, borrowedBook := range member.BorrowedBooks {
+			if borrowedBook.ID == bookID {
+				return errors.New("book is already borrowed")
+			}
+		}
+	}
+
+	member.BorrowedBooks = append(member.BorrowedBooks, book)
+	l.Members[memberID] = member
+	return nil
+}
+
+func (l *Library) ReturnBook(bookID, memberID int) error {
+	member, ok := l.Members[memberID]
+	if !ok {
+		return errors.New("member not found")
+	}
+
+	for i, borrowedBook := range member.BorrowedBooks {
+		if borrowedBook.ID == bookID {
+			member.BorrowedBooks = append(member.BorrowedBooks[:i], member.BorrowedBooks[i + 1: ]...)
+			l.Members[memberID] = member
+			return nil
+		}
+	}
+
+	return errors.New("book not borrowed by user")
+}
+
+
+func (l *Library) ListAvailableBooks() []models.Book {
+	availableBooks := make([]models.Book, 0)
+
+	for _, book := range l.Books {
+		isBorrowed := false
+		for _, member := range l.Members {
+			for _, borrowedBook := range member.BorrowedBooks{
+				if borrowedBook.ID == book.ID {
+					isBorrowed = true
+					break
+				}
+			}
+			if isBorrowed {
+				break
+			}
+			if !isBorrowed {
+				availableBooks = append(availableBooks, book)
+			}
+		}
+	}
+
+	return availableBooks
+
+}
+
+func (l *Library) ListBorrowedBooks(memberID int) []models.Book {
+	member, ok := l.Members[memberID]
+	if !ok {
+		return []models.Book{}
+	}
+	return member.BorrowedBooks
+}
